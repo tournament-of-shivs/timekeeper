7959b59ab9835ea5bbaa028a3712d4761b13c3fe
tournament-of-shivs <35544823+tournament-of-shivs@users.noreply.github.com>
2018-07-18 20:22:16 -0500

    Update time.js
    Formatting output

--- commands/time.js
+++ commands/time.js
@@ -7,11 +7,22 @@ module.exports = (srcPath) => {
         usage: 'time',
         command: state => (args, player) => {
             const timekeeper = state.TimeKeeper;
-            return Broadcast.sayAt(player, 'Current time: ' + timekeeper.config.timeUnits[0] + '-' + timekeeper.getTimeByIndex(0));
+            let time_string = 'Current time: ';
+            for (var x = timekeeper.config.timeUnits.length-1; x >= 0; x--){
+                let temp_time = timekeeper.getTimeByIndex(x);
+                // zero padding
+                if(temp_time.toString().length == 1){
+                    temp_time = "0" + temp_time;    
+                }
+                time_string += temp_time;
+                if(x>0){
+                    time_string += ":";
+                }
+            }
+            return Broadcast.sayAt(player, time_string);
         }
     };
 };
-
 
 
 
